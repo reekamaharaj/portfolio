e862bde0dc73caecaf3e2328e3c89d6f0e4fdc97
reekamaharaj <reekamaharaj@gmail.com>
2020-07-20 23:59:26 -0500

    removed build

--- build/_dist_/components/Portfolio.js
+++
@@ -1,7 +0,0 @@
-import React from "web_modules/react.js";
-import Project1 from "./Project1.js";
-import Project2 from "./Project2.js";
-import Project3 from "./Project3.js";
-export default function Portfolio() {
-  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h2", null, "Portfolio"), /*#__PURE__*/React.createElement(Project1, null), /*#__PURE__*/React.createElement(Project2, null), /*#__PURE__*/React.createElement(Project3, null));
-}
--- build/_dist_/index.css.proxy.js
+++
@@ -1,9 +0,0 @@
-
-const code = "body {\n\tmargin: 0;\n\tfont-family: -apple-system, BlinkMacSystemFont, \"Segoe UI\", \"Roboto\",\n\t\t\"Oxygen\", \"Ubuntu\", \"Cantarell\", \"Fira Sans\", \"Droid Sans\",\n\t\t\"Helvetica Neue\", sans-serif;\n\t-webkit-font-smoothing: antialiased;\n\t-moz-osx-font-smoothing: grayscale;\n}\nhtml, body, #root, #root>div {\n\theight: 100%;\n\twidth:100%;\n\tmargin: 0;\n\tpadding: 0;\n}\ncode {\n\tfont-family: source-code-pro, Menlo, Monaco, Consolas, \"Courier New\",\n\t\tmonospace;\n}\n";
-
-const styleEl = document.createElement("style");
-const codeEl = document.createTextNode(code);
-styleEl.type = 'text/css';
-
-styleEl.appendChild(codeEl);
-document.head.appendChild(styleEl);
