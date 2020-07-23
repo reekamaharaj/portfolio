278dbfa9b533315588359b5454cf8b27f5fd951b
reekamaharaj <reekamaharaj@gmail.com>
2020-07-22 22:22:48 -0500

    json file with project info

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,22 @@
 import { createMuiTheme } from "@material-ui/core/styles";
+import createPalette from "@material-ui/core/styles/createPalette";
+import createTypography from "@material-ui/core/styles/createTypography";
+import "./index.css";
 
-const theme = createMuiTheme({
-	palette: {
-		primary: { 500: "#467fcf" },
-	},
-});
+const theme = (() => {
+	const palette = createPalette({
+		type: "dark",
+	});
+	const typography = createTypography(palette, {
+		fontFamily: "'Anton', sans-serif",
+		fontFamily: "'Dancing Script', cursive",
+		fontFamily: "'Montserrat', sans-serif",
+	});
+
+	return createMuiTheme({
+		palette: palette,
+		typography: typography,
+	});
+})();
+
 export default theme;
